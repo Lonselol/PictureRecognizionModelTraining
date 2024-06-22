08d387c2cb0b2ddc6c152388ce78d1fc741e93d7
Salomanka <oleg65play@gmail.com>
2024-06-22 17:42:23 +1000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
+import os
 import tensorflow as tf
+from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications import MobileNet
 import coremltools as ct
-import os
 
 def main():
-  # Загрузка предобученной модели MobileNet
-  model = MobileNet(weights='imagenet')
+    # Загрузка предобученной модели MobileNet
+    model = MobileNet(weights='imagenet')
 
-  output_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output')
-  output_path = os.path.join(output_dir, 'MobileNet.mlmodel')
+    # Конвертация модели в формат Core ML
+    coreml_model = ct.convert(model)
 
-  # Конвертация модели в формат Core ML
-  coreml_model = ct.convert(model)
-  coreml_model.save(output_path)
+    # Определение пути для сохранения модели
+    output_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output')
+    output_path = os.path.join(output_dir, 'MobileNet.mlmodel')
 
-if __name__ == "__main.py__":
-  main()
+    # Сохранение модели
+    coreml_model.save(output_path)
+
+if __name__ == "__main__":
+    main()
