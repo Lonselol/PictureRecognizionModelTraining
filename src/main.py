640421ef05feef75bdbae48fd314ac2fc31f4d5f
Salomanka <oleg65play@gmail.com>
2024-06-22 18:58:15 +1000

    СУКА БЛЯДЬ

--- src/main.py
+++ src/main.py
@@ -1,21 +1,33 @@
-import os
-from tensorflow.keras.api.applications.mobilenet import MobileNet
+import torchvision
+import torch
 import coremltools as ct
+import os
 
 def main():
+    # Load a pre-trained version of MobileNetV2
+    torch_model = torchvision.models.mobilenet_v2(pretrained=True)
+    # Set the model in evaluation mode.
+    torch_model.eval()
 
-    # Загрузка предобученной модели MobileNet
-    model = MobileNet(weights='imagenet')
+    # Trace the model with random data.
+    example_input = torch.rand(1, 3, 224, 224) 
+    traced_model = torch.jit.trace(torch_model, example_input)
+    out = traced_model(example_input)
 
-    # Конвертация модели в формат Core ML
-    coreml_model = ct.convert(model, source='TensorFlow')
+    # Using image_input in the inputs parameter:
+    # Convert to Core ML using the Unified Conversion API.
+    model = ct.convert(
+        traced_model,
+        inputs=[ct.TensorType(shape=example_input.shape)]
+     )
 
     # Определение пути для сохранения модели
-    #output_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output')
-    #output_path = os.path.join(output_dir, 'MobileNet.mlmodelM')
+    output_path = 'MobileNet.mlmodel'
+    #output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output')
+    #output_path = os.path.join(output_dir, 'MobileNet.mlmodel')
 
     # Сохранение модели
-    coreml_model.save("MobileNet.mlmodel")
+    model.save(output_path)
 
 if __name__ == "__main__":
     main()
