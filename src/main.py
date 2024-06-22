eb6d7797cd8ad0a60d76aad7f6097414727ca9c9
Salomanka <oleg65play@gmail.com>
2024-06-22 17:54:07 +1000

    ц

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import os
 import tensorflow as tf
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.applications import MobileNet
+from keras._tf_keras.keras.applications import MobileNet
 import coremltools as ct
 
 def main():
@@ -9,7 +8,7 @@ def main():
     model = MobileNet(weights='imagenet')
 
     # Конвертация модели в формат Core ML
-    coreml_model = ct.convert(model)
+    coreml_model = ct.convert(model, source='tensorflow')
 
     # Определение пути для сохранения модели
     output_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output')
