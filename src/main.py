8ee11c0f891518bf94f25147c80f2fefc20e9073
Salomanka <oleg65play@gmail.com>
2024-06-22 18:32:40 +1000

    уц

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 import os
-import tensorflow as tf
-from keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.api.applications.mobilenet import MobileNet
 import coremltools as ct
 
 def main():
+
     # Загрузка предобученной модели MobileNet
-    model = MobileNetV2(weights='imagenet')
+    model = MobileNet(weights='imagenet')
 
     # Конвертация модели в формат Core ML
-    coreml_model = ct.convert(model, source='tensorflow')
+    coreml_model = ct.convert(model, source='TensorFlow')
 
     # Определение пути для сохранения модели
-    output_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output')
-    output_path = os.path.join(output_dir, 'MobileNet.mlmodel')
+    #output_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output')
+    #output_path = os.path.join(output_dir, 'MobileNet.mlmodelM')
 
     # Сохранение модели
-    coreml_model.save(output_path)
+    coreml_model.save("MobileNet.mlmodel")
 
 if __name__ == "__main__":
     main()
